973b643db9daecfc8b59907f4dce380e5d086694
Eryk <erykpek123@gmail.com>
2023-09-08 09:32:00 +0200

    zmiana

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wartosc Silni:");
 
-        System.out.printf("Hello and welcome!");
+        int l1 = scanner.nextInt();
+        int l2 = 1;
 
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
+        for(int i=1; i<=l1; i++) {
+            l2 = l1 * i;
         }
+        System.out.println(l2);
     }
 }
